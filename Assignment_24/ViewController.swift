486b8c69a4a342f9700cab3a87146b57a7f48b23
topuchi13 <nicolas1213@gmail.com>
2021-11-15 19:01:58 +0400

    Initial Commit

---
+++ Assignment_24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_24
+//
+//  Created by Nika Topuria on 15.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
